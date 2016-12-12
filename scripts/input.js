9c3c55f418dc70d1322b2b642d95ddc88ab360c0
陆玄青 <lu.xuanqing@outlook.com>
2016-12-12 11:11:57 +0800

    add functions
    add calc()
    add clear()
    display operators
    fix “0” position
    fix result opacity to 0.5

--- scripts/input.js
+++ scripts/input.js
@@ -1,14 +1,39 @@
-function prepareInput() {
 	var expression = document.getElementById('expression')
-	// 设置数字按键
+	var result = document.getElementById('result')
+	// 设置数字输入显示
 	var nums = document.getElementsByClassName('btn-num')
 	for (var i = 0; i < nums.length; i++) {
 		nums[i].onclick = function() {
-			console.log(this.getAttribute('value'))
+			console.log('pressed ' + this.getAttribute('value'))
 			expression.innerHTML += this.getAttribute('value')
+			calc()
 		}
 	}
-}
-window.onload = prepareInput
+	// 设置加减乘除的显示
+	var operators = document.getElementsByClassName('operators')
+	for (var i = 0; i < operators.length; i++) {
+		operators[i].onclick = function() {
+			console.log('pressed ' + this.getAttribute('value'))
+			expression.innerHTML += this.getAttribute('value')
+			calc()
+		}
+	}
 
+	// 结果实时显示
+	function calc() {		
+		var exp_pseudo = expression.innerText
+		console.log(exp_pseudo + '=')
+		result.innerText = eval(expression.innerText)
+	}
 
+	// 设置C按键清除屏幕
+	function clear() {
+		expression.innerText = ''
+		result.innerText = 0
+		console.log('cleared!')
+	}
+	var btnClear = document.getElementById('clear')
+	btnClear.onclick = function() {
+		clear()
+	}
+	
